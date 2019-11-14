3fa56147832bcbcfb2a55bf87482acfd45bd91a4
Patryk Szlachta <patryk.szla@interia.pl>
2019-11-14 09:04:22 +0100

    Updates

---
+++ precache-manifest.9fff470f9d5a0f26ccda8b7a8d011851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726956bc37da010130e1785d71b269c2",
+    "url": "/react-calculator-bit-coin/index.html"
+  },
+  {
+    "revision": "389827039c770087c4aa",
+    "url": "/react-calculator-bit-coin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6e75bf11cdd76328e6d0",
+    "url": "/react-calculator-bit-coin/static/js/2.45e3d12e.chunk.js"
+  },
+  {
+    "revision": "389827039c770087c4aa",
+    "url": "/react-calculator-bit-coin/static/js/main.da766a22.chunk.js"
+  },
+  {
+    "revision": "8acf5392618dc8f5c180",
+    "url": "/react-calculator-bit-coin/static/js/runtime-main.9dfc6670.js"
+  }
+]);
